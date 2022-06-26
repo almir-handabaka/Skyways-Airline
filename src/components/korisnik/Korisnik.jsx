08ef821dfe0fded3b67ca19115620fe1277d8834
almir-handabaka <73104081+almir-handabaka@users.noreply.github.com>
2022-06-27 00:46:03 +0200

    korisnik template

--- src/components/korisnik/Korisnik.jsx
+++ src/components/korisnik/Korisnik.jsx
@@ -9,34 +9,70 @@ import { v4 as uuidv4 } from 'uuid';
 
 const Korisnik = () => {
 
+  const [letovi, setLetovi] = useState([]);
+
   const handleInput = (event) => {
     //const {name, value} = event.target;
     //setNoviRadnik({...noviRadnik, [name]: value});
   }
 
 
+  useEffect(()=> {
+    const getLetove = async () => {
+      setLetovi([]);
+      const q = query(collection(db, "letovi"));
+
+      const querySnapshot = await getDocs(q);
+      setLetovi(querySnapshot.docs.map((doc) => ({ ...doc.data(), id:doc.id }) ));
+    }
+
+    return () => {
+      getLetove();
+    };
+
+  }, [])
 
 
   return (
     <>
       <Container className="m-3">
         <Row className="justify-content-center">
-          <Col xs={10} className="border" >
+          <Col xs={6} className="border p-4" >
             <form>
-              <div class="mb-3">
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Od" />
+              <div className="mb-3">
+                <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Od" />
               </div>
-
-              <div class="mb-3">
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Do" />
+              <div className="mb-3">
+                <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Do" />
               </div>
-              <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+              <div className="mb-3">
+                <input type="date" className="form-control" id="exampleFormControlInput1" placeholder="Datum" />
               </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary mx-5">Pretraga</button>
             </form>
           </Col>
+        </Row>
+
+        <hr class="border-primary border-3 opacity-75" />
+
+        <Row className="justify-content-center">
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+        {
+            letovi && letovi.map((let_info) => {
+                return <div class="col">
+                    <div class="card">
+                      <img src="..." class="card-img-top" alt="..." />
+                      <div class="card-body">
+                        <h5 class="card-title">{let_info.id}</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      </div>
+                    </div>
+                  </div>
+                
+
+            })
+        }
+          </div>
         </Row>
       </Container>
     </>
--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
@@ -10,7 +10,23 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId
 };
+*/
 
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAnDnK9ox_vhuYg5aafzfyUtpg3zLaG-Aw",
+
+  authDomain: "dws-skyways.firebaseapp.com",
+
+  projectId: "dws-skyways",
+
+  storageBucket: "dws-skyways.appspot.com",
+
+  messagingSenderId: "86170670538",
+
+  appId: "1:86170670538:web:df579a3af70dbc0b186b33"
+
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
